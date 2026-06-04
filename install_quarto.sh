98ce7cf37a7a335646d45e5c0d551775c4ccdb4c
Erwin Lares <erwin.lares@wisc.edu>
2026-06-04 13:08:16 -0500

    fix: extract quarto to HOME to avoid rendering template file

--- install_quarto.sh
+++ install_quarto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-wget -qO quarto.tar.gz "https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz"
-tar -xzf quarto.tar.gz
+wget -qO /tmp/quarto.tar.gz "https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz"
+tar -xzf /tmp/quarto.tar.gz -C $HOME
 mkdir -p $HOME/.local/bin
-ln -s "$(pwd)/quarto-${QUARTO_VERSION}/bin/quarto" $HOME/.local/bin/quarto
+ln -sf "$HOME/quarto-${QUARTO_VERSION}/bin/quarto" $HOME/.local/bin/quarto
 export PATH=$HOME/.local/bin:$PATH
 quarto render
