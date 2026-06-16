507a27de8bd9ddef2437e2f3ea0d252239f16637
Erwin Lares <erwin.lares@wisc.edu>
2026-06-16 14:34:26 -0500

    matching the ubuntu version to what I see on netlify

--- install_r.sh
+++ install_r.sh
@@ -2,11 +2,10 @@
 set -e
 
 R_VERSION=${R_VERSION:-"4.4.2"}
-R_MAJOR=$(echo $R_VERSION | cut -d. -f1)
 
-wget -qO /tmp/R.tar.gz "https://cdn.rstudio.com/r/ubuntu-2204/pkgs/r-${R_VERSION}_1_amd64.deb"
+wget -qO /tmp/R.deb "https://cdn.rstudio.com/r/ubuntu-2404/pkgs/r-${R_VERSION}_1_amd64.deb"
 mkdir -p $HOME/R-${R_VERSION}
-dpkg-deb -x /tmp/R.tar.gz $HOME/R-${R_VERSION}
+dpkg-deb -x /tmp/R.deb $HOME/R-${R_VERSION}
 
 mkdir -p $HOME/.local/bin
 ln -sf "$HOME/R-${R_VERSION}/opt/R/${R_VERSION}/bin/R" $HOME/.local/bin/R
